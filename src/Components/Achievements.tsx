bc75c331d9c33a25c22a462f8971e663bda086da
LinusTC <tongchun03@gmail.com>
2024-05-03 10:44:01 +0100

    small text added

--- src/Components/Achievements.tsx
+++ src/Components/Achievements.tsx
@@ -5,40 +5,35 @@ export default function MainSection() {
   const cardData = [
     {
       title: "Item1",
-      src:
-        "https://placehold.co/1920x1080",
+      src: "https://placehold.co/1920x1080",
     },
     {
       title: "Item2",
-      src:
-        "https://placehold.co/1920x1080",
+      src: "https://placehold.co/1920x1080",
     },
     {
       title: "Item3",
-      src:
-        "https://placehold.co/1920x1080",
+      src: "https://placehold.co/1920x1080",
     },
-    
   ].map((cardObj, index) => {
     return (
-      <Card
-        key={`card-${index}`}
-        title={cardObj.title}
-        src={cardObj.src}
-      />
+      <Card key={`card-${index}`} title={cardObj.title} src={cardObj.src} />
     );
   });
 
   return (
-    <motion.div
-      className="grid grid-cols-2 gap-4"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{
-        delay: 1,
-      }}
-    >
-      {cardData}
-    </motion.div>
+    <>
+      <p className="pb-2 text-white text-lg">Some of my latest achievements:</p>
+      <motion.div
+        className="grid grid-cols-2 gap-4"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 1,
+        }}
+      >
+        {cardData}
+      </motion.div>
+    </>
   );
 }
